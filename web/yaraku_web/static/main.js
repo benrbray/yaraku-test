fe0cce3d9e4ca7a10e9033653aaea3f7fc477f30
Benjamin R. Bray <benrbray@umich.edu>
2020-02-15 17:35:37 -0500

    enable automatic refresh of book list

--- web/yaraku_web/static/main.js
+++ web/yaraku_web/static/main.js
@@ -1,6 +1,8 @@
 let formAddBook = null;
+let bookTableBody = null;
 
 window.onload = function(){
+	bookTableBody = document.getElementById("bookTableBody");
 	formAddBook = document.getElementById("formAddBook");
 	formAddBook.onsubmit = handleAddBook;
 }
@@ -24,8 +26,31 @@ function refreshBookList(){
 	request.send();
 }
 
+function makeBookTableRow(book){
+	let row = document.createElement("tr");
+	
+	let td_title = document.createElement("td");
+	td_title.innerText = book.title;
+	row.appendChild(td_title);
+
+	let td_author = document.createElement("td");
+	td_author.innerText = book.author;
+	row.appendChild(td_author);
+
+	return row;
+}
+
 function displayBookList(bookList){
-	console.log(bookList);
+	// clear previous table
+	// TODO:  re-use old table rows
+	while(bookTableBody.firstChild){
+		bookTableBody.removeChild(bookTableBody.firstChild);
+	}
+
+	// create new table rows
+	bookList.forEach(book => {
+		bookTableBody.appendChild(makeBookTableRow(book))
+	});	
 }
 
 //// EVENT HANDLERS //////////////////////////////////////////////////
@@ -40,10 +65,12 @@ function handleAddBook(event){
 	
 	request.onload = function(){
 		// TODO: handle response to book add
+		refreshBookList();
 	}
 
 	request.onerror = function(){
 		// TODO: handle errors in book list request
+		refreshBookList();
 	}
 
 	// build request data
@@ -51,7 +78,7 @@ function handleAddBook(event){
 		title  : formAddBook.elements.title.value,
 		author : formAddBook.elements.author.value
 	}
-	
+
 	request.send(JSON.stringify(message));
 
 	return false;
