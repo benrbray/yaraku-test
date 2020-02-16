51002bbe2e62c44eed866f541db82756884fa516
Benjamin R. Bray <benrbray@umich.edu>
2020-02-16 09:18:59 -0500

    disable flask debug mode

--- web/home.py
+++ web/home.py
@@ -1,7 +1,8 @@
 import yaraku_web;
+import time;
 
 if __name__ == "__main__":
 	# run flask in debug mode
 	print("Starting YARAKU BOOKS...");
 	app = yaraku_web.create_app();
-	app.run(host="0.0.0.0", debug=True);
+	app.run(host="0.0.0.0");
